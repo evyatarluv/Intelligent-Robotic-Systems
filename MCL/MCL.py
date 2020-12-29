Fix: speed-up measurement probability computation time

MCL/MCL.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import numpy as np
+from scipy.stats import norm
 from tqdm import tqdm
 from .Robot import Robot
 import matplotlib.pyplot as plt
@@ -16,6 +17,7 @@ class MCL:
         particles (list of Robot): The (resampled) particles of the current step.
         estimated_robot (Robot): The estimated Robot the algorithm computed to the current step, represent
                                     where the algorithm believe the robot's location.
+        distributions (dict): Dict which include the error distributions of each sensor (created for speeding run time)
     """
 
     def __init__(self, robot, landmarks, m):
@@ -30,6 +32,8 @@ class MCL:
         self.particles = []
         self.estimated_robot = deepcopy(robot)
         self.m = m
+        self.distributions = {'range': norm(0, robot.noise_std['range']),
+                              'bearing': norm(0, robot.noise_std['bearing'])}
 
     def localize(self, motion_commands, measurements, plot=True):
         """
@@ -85,7 +89,8 @@ class MCL:
 
             # Compute the probability & append it
             landmark_prob = particle.measurement_probability(measurement=measurements[landmark_index],
-                                                             landmark=self.landmarks[landmark_index])
+                                                             landmark=self.landmarks[landmark_index],
+                                                             distributions=self.distributions)
             prob.append(landmark_prob)
 
         return np.prod(prob)

MCL/Robot.py
@@ -162,10 +162,11 @@ class Robot:
 
         return measurements
 
-    def measurement_probability(self, measurement, landmark):
+    def measurement_probability(self, measurement, landmark, distributions=None):
         """
         The method compute the probability for a given measurement to be observed when being
         in a given pose.
+        :param distributions: dict, distributions of each measure. For speeding up calculation time.
         :param landmark: landmark position, (x,y)
         :param measurement: measurement the robot measure, (range, bearing)
         :return: float, probability between 0 to 1
@@ -177,8 +178,12 @@ class Robot:
         m_x, m_y = landmark
 
         # Create normal distributions according to the range & bearing std
-        r_dist = norm(0, self.noise_std['range'])
-        phi_dist = norm(0, self.noise_std['bearing'])
+        if distributions is None:
+            r_dist = norm(0, self.noise_std['range'])
+            phi_dist = norm(0, self.noise_std['bearing'])
+        else:
+            r_dist = distributions['range']
+            phi_dist = distributions['bearing']
 
         # Compute measurement giving the pose
         meas_r = np.sqrt((m_x - x) ** 2 + (m_y - y) ** 2)