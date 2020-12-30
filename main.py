8e20a23f38c2c8ed5643ef14d346e10fe16c17f2
luvaton <evyatarluv@gmail.com>
2020-12-30 10:13:26 +0200

    Update: main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import numpy as np
 import pandas as pd
 
 # --------------------------------------------------------------------------------------------------------
+# ----------------------------Example for using the MCL class --------------------------------------------
+# --------------------------------------------------------------------------------------------------------
 # world = World()
 # robot = Robot(init_pose=(10, 15, 0), noise_std={'forward': 6, 'turn': 0.1, 'range': 5, 'bearing': 0.3})
-# ideal_robot = Robot(init_pose=(10, 15, 0))
 # moves = [(0, 60), (np.pi / 3, 30), (np.pi / 4, 30), (np.pi / 4, 20), (np.pi / 4, 40)]
 # mcl = MCL(robot, world.landmarks, 1000)
 #
@@ -21,13 +22,11 @@ import pandas as pd
 #
 # for u in moves:
 #
-#     ideal_robot.move(u[0], u[1])
 #     robot.move(u[0], u[1])
 #     mcl.localize(u, robot.sense(world.landmarks))
 #     robot.plot()
 #
 # # Plot paths
-# plt.plot(*(zip(*ideal_robot.path)), '-.', color='gold', label='Commands')
 # plt.plot(*(zip(*mcl.path)), '-.', color='limegreen', label='MCL')
 # plt.plot(*(zip(*robot.path)), '-.', color='royalblue', label='Robot')
 # plt.legend(bbox_to_anchor=(0.98, 1), loc='upper left')
