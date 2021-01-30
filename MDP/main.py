6d9722a26cbd3f7ae1f4acb6d3da19b8736abc30
luvaton <evyatarluv@gmail.com>
2021-01-30 21:50:23 +0200

    add: MDP files

--- MDP/main.py
+++ MDP/main.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-
-from World import World
+from MDP.World import World
 import numpy as np
-
 
 if __name__ == "__main__":
 
-     world = World()
-     world.plot()
-     world.plot_value([np.random.random() for i in range(world.nStates)])
-     world.plot_policy(np.random.randint(1, world.nActions,(world.nStates, 1)))
-
+    world = World()
+    world.plot()
+    world.plot_value([np.random.random() for i in range(world.nStates)])
+    world.plot_policy(np.random.randint(1, world.nActions, (world.nStates, 1)))
