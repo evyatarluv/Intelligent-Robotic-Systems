6e3132ec7ba132b19a0cf4040011363ab08f7a61
luvaton <evyatarluv@gmail.com>
2021-01-31 16:06:03 +0200

    Update: Report

--- examples/mdp.py
+++ examples/mdp.py
@@ -1,11 +1,33 @@
-from MDP.utilitiy import reward_function
+from MDP.utilitiy import reward_function, TransitionModel
+import pickle
+
+
+def load_matrices():
+    """
+    Load the probability matrix for each action.
+    :return:
+    """
+    actions = ['north', 'east', 'south', 'west']
+    matrices = []
+
+    for a in actions:
+        path = '../MDP/transition_model/{}.pkl'.format(a)
+        matrices.append(pickle.load(open(path, 'rb')))
+        # print('\nAction = {} \n{}'.format(a.capitalize(), matrices[-1]))
+
+    return matrices
 
 
 def main():
-    rewards = reward_function(holes=[1, 7, 14],
-                              goals=[13],
-                              obstacles=[15])
+    actions = ['north', 'east', 'south', 'west']
+    prob_matrices = load_matrices()
+
+    # Transition model & reward function
+    transition_model = TransitionModel(actions=[a[0] for a in actions],
+                                       transition_matrices=prob_matrices)
+
+    rewards = reward_function(holes=[1, 7, 14], goals=[13], obstacles=[15])
+
 
 if __name__ == '__main__':
-
     main()
